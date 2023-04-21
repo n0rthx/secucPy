2ef0d68467bfad2644f66f84f5610b7c34988c93
Furkan <furkanaaslan07@outlook.com>
2023-04-21 23:00:36 +0300

    FileTransfer class and SOCK_DGRAM_TRANSFER method added!

--- secucpy/client/transfer.py
+++ secucpy/client/transfer.py
@@ -8,20 +8,22 @@ import configparser
 from numba import jit
 
 
-def classic_client_transfer(host:str, port:int, message, recvValue:int = 1024, _return:bool = False):
+class Create:
+
+    def classic_client_transfer(host:str, port:int, message, recvValue:int = 1024, _return:bool = False):
  
     # it is use classic transfer method
 
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sct: 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sct: 
         
-        sct.connect((host, port))
-        sct.sendall(message)
+            sct.connect((host, port))
+            sct.sendall(message)
 
-        if _return == True:
+            if _return == True:
             
-            data = sct.recv(recvValue)
-            return data
+                data = sct.recv(recvValue)
+                return data
 
 
 
@@ -63,7 +65,7 @@ def client_detailed_transfer(host:str, port:int, message, log:bool = True, _retu
 
 
 
-def config_transfer(message, ConfigFile:str, serverInfoName:str = "SERVER_INFO", connectMethodsInfo:str = "CONNECT_METHOD"):
+def config_transfer(message, ConfigFile:str, serverInfoName:str = "SERVER_INFO", connectMethodsInfo:str = "CONNECT_METHOD", SocketKind = socket.SOCK_STREAM):
     
     config = configparser.ConfigParser()
     config.read(ConfigFile)
@@ -74,13 +76,12 @@ def config_transfer(message, ConfigFile:str, serverInfoName:str = "SERVER_INFO",
 
     # GET CONNECTION METHODS INFO 
 
-    sock = config[connectMethodsInfo].get['SOCK_FAMILY']
     recv = config[connectMethodsInfo].getint['RECV']
     _rec = config[connectMethodsInfo].getboolean['REC_CHECK']
  
 
     try:
-        with socket.socket(socket.AF_INET, sock) as sct : 
+        with socket.socket(socket.AF_INET, SocketKind) as sct : 
             sct.connect((host, port))
             sct.sendall(message)
 
@@ -94,7 +95,7 @@ def config_transfer(message, ConfigFile:str, serverInfoName:str = "SERVER_INFO",
             
             
     except:
-            with socket.socket(socket.AF_INET6, sock) as sct : 
+            with socket.socket(socket.AF_INET6, SocketKind) as sct : 
                 sct.connect((host, port))
                 sct.sendall(message)
 
@@ -105,6 +106,75 @@ def config_transfer(message, ConfigFile:str, serverInfoName:str = "SERVER_INFO",
                 else:
 
                     return None
+                
+
+def SOCK_DGRAM_TRANSFER(host:str, port:int, message, recv:int, rec:bool = False): 
+
+            # DGRAM Transfer Method
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sct:
+                sct.connect((host, port))
+                sct.sendall(message)
+
+                if rec != False:
+                    
+                    data = sct.recv(recv)
+                    return print(data)
+                
+                else:
+                    return None
+        
+        except:
+
+                with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sct:
+                    sct.connect((host, port))
+                    sct.sendall(message)
+
+                    if rec != False:
+                    
+                        data = sct.recv(recv)
+                        return data
+
+                    else:
+                        return None
+                    
+
+
+class FileTransfer:
+    
+    
+    def classic_FileTransfer(filePath, host:str, port:int, recv:int = 1024):
+         
+
+         # Classic file transfer method
+         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sct:
+              
+              sct.connect((host, port))
+
+              sendFile = open(filePath, 'rb')
+              sct.sendfile(sendFile)
+
+
+
+
+         
+
+        
+
+
+
+                    
+        
+
+
+                    
+
+        
+                
+
+
+                
+            
 
 
 
